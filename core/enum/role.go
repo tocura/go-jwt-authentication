582641814c9623d9e57bbbeaa288d4846bb39426
Mauricio Tocura <mryutoc@gmail.com>
2022-06-05 10:57:28 -0300

    feat: Create mapper to role enum

--- core/enum/role.go
+++ core/enum/role.go
@@ -6,3 +6,12 @@ const (
 	RolePremium EnumRole = "premium"
 	RoleNormal  EnumRole = "normal"
 )
+
+func MapToEnumRole(role string) EnumRole {
+	m := map[string]EnumRole{
+		"premium": RolePremium,
+		"normal":  RoleNormal,
+	}
+
+	return m[role]
+}
